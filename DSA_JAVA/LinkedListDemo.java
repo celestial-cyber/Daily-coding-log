f437b638e5e0493dc60ba35c132f8b776a226b3f
farheenNisha <farheennisha1405@gmail.com>
2026-06-01 21:31:57 +0530

    added remove , reverse and insert method in linkedlist

--- DSA_JAVA/LinkedListDemo.java
+++ DSA_JAVA/LinkedListDemo.java
@@ -1,3 +1,6 @@
+
+import javax.sound.midi.InvalidMidiDataException;
+
 // LinkedListDemo.java
 // This class implements a singly linked list using the Node class
 
@@ -102,7 +105,77 @@ public class LinkedListDemo {
         }
         return false;
     }
+    //inserting at a specified position
+    public boolean insert(int index, int value) {
 
+    if (index < 0 || index > length) {
+        return false;
+    }
+
+    if (index == 0) {
+        prepend(value);
+        return true;
+    }
+
+    if (index == length) {
+        append(value);
+        return true;
+    }
+
+    Node newNode = new Node(value);
+
+    Node temp = get(index - 1);
+
+    newNode.next = temp.next;
+    temp.next = newNode;
+
+    length++;
+
+    return true;
+}
+
+public Node remove(int index) {
+
+    if (index < 0 || index >= length) {
+        return null;
+    }
+
+    if (index == 0) {
+        return removeFirst();
+    }
+
+    if (index == length - 1) {
+        return removeLast();
+    }
+
+    Node prev = get(index - 1);
+    Node temp = prev.next;
+
+    prev.next = temp.next;
+    temp.next = null;
+
+    length--;
+
+    return temp;
+}
+
+//reverse the list 
+public void reverse() {
+
+    Node temp = head;
+    head = tail;
+    tail = temp;
+
+    Node after = temp.next;
+    Node before = null;
+
+    for (int i = 0; i < length; i++) {
+        after = temp.next;
+        temp.next = before;
+        before = temp;
+        temp = after;
+    }
+}
     // Main method for testing
     public static void main(String[] args) {
         // Create linked list with initial value 41
